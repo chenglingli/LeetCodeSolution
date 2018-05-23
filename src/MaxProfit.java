b98474846a9b715f8ee4c7c33d37ae0325bed643
Solomon Li <chengling.lish@ele.me>
2018-05-23 14:52:40 +0800

    refine

--- src/MaxProfit.java
+++ src/MaxProfit.java
@@ -4,42 +4,38 @@ public class MaxProfit {
 
     public int maxProfit2(int[] prices) {
 
-        int max= 0 ;
-        if(prices.length==0)
-            return 0 ;
+        int max = 0;
+        if (prices.length == 0)
+            return 0;
         int minbuyprice = prices[0];
 
-        for(int i = 0 ;i < prices.length  ;i++)
-        {
-            if(prices[i]<minbuyprice )
-                minbuyprice =prices[i];
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minbuyprice)
+                minbuyprice = prices[i];
             else
                 //   max = Math.max(prices[i]-minbuyprice,max);
-                max = max > prices[i]-minbuyprice ? max : prices[i]-minbuyprice ;
-
-
+                max = max > prices[i] - minbuyprice ? max : prices[i] - minbuyprice;
         }
-        return max ;
+        return max;
 
     }
 
     public int maxProfit(int[] prices) {
 
         // 从尾部计算最大售出价
-        int [] max = new int [prices.length];
+        int[] max = new int[prices.length];
         int curMax = -1;
-        for (int i = prices.length-1; i >= 0 ; i--) {
+        for (int i = prices.length - 1; i >= 0; i--) {
             if (prices[i] > curMax) {
                 max[i] = prices[i];
                 curMax = max[i];
-            }
-            else {
+            } else {
                 max[i] = curMax;
             }
         }
 
         int curRes = 0;
-        for (int i=0 ; i< prices.length ; i++) {
+        for (int i = 0; i < prices.length; i++) {
             int temp = max[i] - prices[i];
             if (temp > curRes) {
                 curRes = temp;
@@ -53,7 +49,7 @@ public class MaxProfit {
         MaxProfit s = new MaxProfit();
 
         long sysDate1 = System.currentTimeMillis();
-        int [] prices = {7};
+        int[] prices = {7};
 
         int res = s.maxProfit(prices);
         System.out.println(res);
