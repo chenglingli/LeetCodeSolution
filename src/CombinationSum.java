06c4ec799b34214558f908c3be37b463e0ffe026
Solomon Li <chengling.lish@ele.me>
2019-05-07 15:45:55 +0800

    Combination Sum

--- src/CombinationSum.java
+++ src/CombinationSum.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Solution {
+public class CombinationSum {
 
 
     List<List<Integer>> res = new ArrayList<>();
@@ -37,7 +37,7 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        Solution s = new Solution();
+        CombinationSum s = new CombinationSum();
 
         long sysDate1 = System.currentTimeMillis();
 
