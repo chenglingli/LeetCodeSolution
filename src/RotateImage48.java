a28cb266822c1bfe103d75a5c41400d8d05172f7
Solomon Li <chengling.lish@ele.me>
2019-05-21 18:39:27 +0800

    48. Rotate Image

--- src/RotateImage48.java
+++ src/RotateImage48.java
@@ -1,8 +1,4 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
-
-public class Solution {
+public class RotateImage48 {
 
     public void rotate(int[][] matrix) {
 
@@ -21,7 +17,7 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        Solution s = new Solution();
+        RotateImage48 s = new RotateImage48();
 
         long sysDate1 = System.currentTimeMillis();
 
