779bd1972729da3719f46a827fc20f6630a61063
Solomon Li <chengling.lish@ele.me>
2019-06-14 11:29:00 +0800

    deleteDuplicates

--- src/deleteDuplicates82.java
+++ src/deleteDuplicates82.java
@@ -1,9 +1,4 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class Solution {
+public class deleteDuplicates82 {
 
     public ListNode deleteDuplicates(ListNode head) {
 
@@ -43,7 +38,7 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        Solution s = new Solution();
+        deleteDuplicates82 s = new deleteDuplicates82();
 
         long sysDate1 = System.currentTimeMillis();
 
