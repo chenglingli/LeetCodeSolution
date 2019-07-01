89b47a45f335768fc9a348521b032c6729f2daed
Solomon Li <chengling.lish@ele.me>
2019-07-01 15:04:19 +0800

    update name

--- src/L237_Delete_Node_in_a_Linked_List.java
+++ src/L237_Delete_Node_in_a_Linked_List.java
@@ -1,4 +1,4 @@
-public class L0237_Delete_Node_in_a_Linked_List {
+public class L237_Delete_Node_in_a_Linked_List {
 
     public void deleteNode(ListNode node) {
 
@@ -13,7 +13,7 @@ public class L0237_Delete_Node_in_a_Linked_List {
 
     public static void main(String[] args) {
 
-        L0237_Delete_Node_in_a_Linked_List s = new L0237_Delete_Node_in_a_Linked_List();
+        L237_Delete_Node_in_a_Linked_List s = new L237_Delete_Node_in_a_Linked_List();
 
         long sysDate1 = System.currentTimeMillis();
 
