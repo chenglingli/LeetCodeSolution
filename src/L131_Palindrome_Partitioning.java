25c83057a2e8ffedfa88baf098662fec1673193b
lichengling <solomon-li@outlook.com>
2021-06-08 14:07:11 +0800

    refine

--- src/L131_Palindrome_Partitioning.java
+++ src/L131_Palindrome_Partitioning.java
@@ -55,18 +55,20 @@ public class L131_Palindrome_Partitioning {
 
 
     public List<List<String>> partition2(String s) {
+
         boolean[][] isPalindrome = new boolean[s.length()][s.length()];
         isPalindrome[0][0] = true;
         for (int i = 1; i < s.length(); i++) {
-            expand(s, i - 1, i, isPalindrome);
-            expand(s, i, i, isPalindrome);
+            init(s, i - 1, i, isPalindrome);
+            init(s, i, i, isPalindrome);
         }
-        List<List<String>> palindromes = new ArrayList<>();
-        partition(isPalindrome, s, 0, new LinkedList<>(), palindromes);
-        return palindromes;
+
+        List<List<String>> res = new ArrayList<>();
+        partition(isPalindrome, s, 0, new LinkedList<>(), res);
+        return res;
     }
 
-    private void expand(String s, int i, int j, boolean[][] isPalindrome) {
+    private void init(String s, int i, int j, boolean[][] isPalindrome) {
         while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
             isPalindrome[i][j] = true;
             i--;
@@ -76,28 +78,32 @@ public class L131_Palindrome_Partitioning {
 
     private void partition(
             boolean[][] isPalindrome,
-            String s, int index,
-            LinkedList<String> palindrome,
-            List<List<String>> palindromes) {
+            String s,
+            int index,
+            LinkedList<String> tmp,
+            List<List<String>> res) {
+
         if (index == s.length()) {
-            palindromes.add(new ArrayList<>(palindrome));
-        } else {
-            for (int i = index; i < s.length(); i++) {
-                if (isPalindrome[index][i]) {
-                    palindrome.addLast(s.substring(index, i + 1));
-                    partition(isPalindrome, s, i + 1, palindrome, palindromes);
-                    palindrome.removeLast();
-                }
+            res.add(new ArrayList<>(tmp));
+            return;
+        }
+
+        for (int i = index; i < s.length(); i++) {
+            if (isPalindrome[index][i]) {
+                tmp.addLast(s.substring(index, i + 1));
+                partition(isPalindrome, s, i + 1, tmp, res);
+                tmp.removeLast();
             }
         }
     }
+
 
     public static void main(String[] args) {
 
         L131_Palindrome_Partitioning s = new L131_Palindrome_Partitioning();
 
         long sysDate1 = System.currentTimeMillis();
-        List<List<String>> res = s.partition("aabbc");
+        List<List<String>> res = s.partition2("aabbc");
 
         for (List<String> l : res) {
             System.out.println(l);
