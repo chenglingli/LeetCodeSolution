51444f0be77eaf36801b9c4505d90acb0b4ea182
Solomon Li <solomon-li@outlook.com>
2021-08-19 15:06:30 +0800

    L102_Binary_Tree_Level_Order_Traversal

---
+++ src/L102_Binary_Tree_Level_Order_Traversal.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class L102_Binary_Tree_Level_Order_Traversal {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> tmp = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                tmp.add(cur.val);
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            res.add(tmp);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        L102_Binary_Tree_Level_Order_Traversal s = new L102_Binary_Tree_Level_Order_Traversal();
+        long sysDate1 = System.currentTimeMillis();
+
+        // [2,null,3,null,4,null,5,null,6]
+
+        // init data
+        TreeNode root = new TreeNode(3);
+        TreeNode root1 = new TreeNode(9);
+        TreeNode root2 = new TreeNode(20);
+        TreeNode root3 = new TreeNode(15);
+        TreeNode root4 = new TreeNode(7);
+        TreeNode root5 = new TreeNode(5);
+        TreeNode root6 = new TreeNode(6);
+        TreeNode root7 = new TreeNode(7);
+        TreeNode root8 = new TreeNode(8);
+
+        root.right = root1;
+        root.left = root2;
+        root1.right = root3;
+        root1.left = root4;
+
+        System.out.println(s.levelOrder(root));
+
+        long sysDate2 = System.currentTimeMillis();
+        System.out.println("\ntime ");
+        System.out.print(sysDate2 - sysDate1);
+    }
+}
