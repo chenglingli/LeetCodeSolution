6fee4a585b9de50da3bd85de23f28e3415d17c89
Solomon Li <solomon-li@outlook.com>
2021-11-05 15:28:20 +0800

    L208_Course_Schedule_II

--- src/L208_Course_Schedule_II.java
+++ src/L208_Course_Schedule_II.java
@@ -1,42 +1,13 @@
 import java.util.*;
 
-public class L207_Course_Schedule {
+public class L208_Course_Schedule_II {
 
-    /*
-
-    There are a total of numCourses courses you have to take,
-    labeled from 0 to numCourses - 1. You are given an array
-    prerequisites where prerequisites[i] = [ai, bi] indicates that
-    you must take course bi first if you want to take course ai.
-
-    For example, the pair [0, 1], indicates that to take course 0
-    you have to first take course 1.
-    Return true if you can finish all courses. Otherwise, return false.
-
-
-
-    Example 1:
-
-    Input: numCourses = 2, prerequisites = [[1,0]]
-    Output: true
-    Explanation: There are a total of 2 courses to take.
-    To take course 1 you should have finished course 0. So it is possible.
-    Example 2:
-
-    Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
-    Output: false
-    Explanation: There are a total of 2 courses to take.
-    To take course 1 you should have finished course 0,
-    and to take course 0 you should also have finished course 1.
-    So it is impossible.
-
-     */
-
-    public boolean canFinish2(int numCourses, int[][] prerequisites) {
-
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
         //邻接表，存放的是指向该节点的其他节点编号，而不是边
         //如果某个点没有其他点指向它，那么对应的list为空。
         HashMap<Integer, List<Integer>> map = new HashMap(numCourses);
+
+        int[] res = new int [numCourses];
 
         for(int i = 0;i < numCourses;i++)
             map.put(i, new ArrayList());
@@ -49,6 +20,7 @@ public class L207_Course_Schedule {
             list.add(previous);
         }
 
+        int i = 0;
         while(map.size() > 0)
         {
             //判断是否能找到一个没有任何节点指向的节点
@@ -63,9 +35,10 @@ public class L207_Course_Schedule {
                 }
 
             if(!flag)
-                return false;
+                return new int[0];
 
             map.remove(num);
+            res[i++] = num;
 
             Iterator<List<Integer>> it = map.values().iterator();
             while(it.hasNext()) {
@@ -73,20 +46,23 @@ public class L207_Course_Schedule {
             }
         }
 
-        return true;
+        return res;
     }
 
     public static void main(String[] args) {
 
-        L207_Course_Schedule s = new L207_Course_Schedule();
+        L208_Course_Schedule_II s = new L208_Course_Schedule_II();
 
         long sysDate1 = System.currentTimeMillis();
 
-        // int[][] prerequisites = {{1, 0} , {0, 2} , {2, 3}};
-        int[][] prerequisites = {{1, 0}};
+        int[][] prerequisites = {{1, 0} , {0, 2} , {2, 3}};
+        // int[][] prerequisites = {{1, 0}, {0, 1}};
 
-        boolean res = s.canFinish2(2, prerequisites);
-        System.out.println(res);
+        int[] res = s.findOrder(4, prerequisites);
+        for (int i = 0 ;i < res.length; i++) {
+            System.out.println(res[i]);
+        }
+
 
         long sysDate2 = System.currentTimeMillis();
         System.out.println("\ntime ");
