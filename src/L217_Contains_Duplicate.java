6d256259aed1f3b1709cd7f2858f7734d0611b37
Solomon Li <solomon-li@outlook.com>
2022-03-07 16:06:25 +0800

    L217_Contains_Duplicate

---
+++ src/L217_Contains_Duplicate.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class L217_Contains_Duplicate {
+
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0 ; i< nums.length ; i++) {
+            set.add(nums[i]);
+        }
+        return set.size() != nums.length;
+    }
+
+    public static void main(String[] args) {
+
+        L217_Contains_Duplicate s = new L217_Contains_Duplicate();
+
+        long sysDate1 = System.currentTimeMillis();
+
+        int[] input = {1, 2, 3, 4, 5, 6, 7, 8, 1};
+        boolean res = s.containsDuplicate(input);
+        System.out.println(res);
+
+        long sysDate2 = System.currentTimeMillis();
+        System.out.println("\ntime ");
+        System.out.print(sysDate2 - sysDate1);
+    }
+}
