f2ea4f075c4e3bd4ba3f3ea3452661bb10e3e364
Solomon Li <solomon-li@outlook.com>
2022-03-10 19:11:43 +0800

    L219_Contains_Duplicate_II

---
+++ src/L219_Contains_Duplicate_II.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class L219_Contains_Duplicate_II {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        if (nums.length == 0 || k ==0 ) {
+            return false;
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0 ; i< nums.length ; i++) {
+
+            Integer x = map.get(nums[i]);
+            if (x == null) {
+                map.put(nums[i], i);
+            }
+            else {
+                if (i - x <= k) {
+                    return true;
+                }
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        L219_Contains_Duplicate_II s = new L219_Contains_Duplicate_II();
+
+        long sysDate1 = System.currentTimeMillis();
+
+        int[] input = {1,2,3,1,2,3};
+        boolean res = s.containsNearbyDuplicate(input, 2);
+        System.out.println(res);
+
+        long sysDate2 = System.currentTimeMillis();
+        System.out.println("\ntime ");
+        System.out.print(sysDate2 - sysDate1);
+    }
+}
