aaf600dbc95d91bca9da84bd5870b4238faad49b
Solomon Li <solomon-li@outlook.com>
2022-05-17 22:30:07 +0800

    L263_Ugly_Number

---
+++ src/L263_Ugly_Number.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class L263_Ugly_Number {
+
+    public boolean isUgly(int n) {
+
+        if (n <= 0 )
+            return false;
+
+        while (n % 5 == 0)
+            n /= 5;
+
+        while (n % 3 == 0)
+            n /= 3;
+
+        while (n % 2 == 0)
+            n /= 2;
+
+        return n == 1;
+
+    }
+
+    public static void main(String[] args) {
+
+        L263_Ugly_Number s = new L263_Ugly_Number();
+        long sysDate1 = System.currentTimeMillis();
+
+        boolean res = s.isUgly(6);
+        System.out.println(res);
+
+        long sysDate2 = System.currentTimeMillis();
+        System.out.println("\ntime ");
+        System.out.print(sysDate2 - sysDate1);
+    }
+}
