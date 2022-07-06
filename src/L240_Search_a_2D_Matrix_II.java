faf0ba55893af96e226dbe862dc89f8ce2f4e7bc
Solomon Li <solomon-li@outlook.com>
2022-07-06 15:36:55 +0800

    240. Search a 2D Matrix II

---
+++ src/L240_Search_a_2D_Matrix_II.java
@@ -0,0 +1,60 @@
+public class L240_Search_a_2D_Matrix_II {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i = 0 ; i < m; i++ ) {
+            if (matrix[i][0] > target || matrix[i][n-1] < target) {
+                continue;
+            }
+
+            if (binarySearch(matrix, target, i)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    boolean binarySearch(int[][] matrix, int target, int line) {
+
+        int l = 0; int r = matrix[line].length-1;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (matrix[line][mid] == target) {
+                return true;
+            }
+            else if (target > matrix[line][mid]) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+
+        L240_Search_a_2D_Matrix_II s = new L240_Search_a_2D_Matrix_II();
+
+        long sysDate1 = System.currentTimeMillis();
+
+        int[][] matrix = {{1,4,7,11,15},{3,5,8,12,19},{5,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        // int[][] matrix = {{1}};
+        int target = 2;
+
+        boolean res = s.searchMatrix(matrix, target);
+
+        System.out.println(res);
+
+        long sysDate2 = System.currentTimeMillis();
+        System.out.println("\ntime ");
+        System.out.print(sysDate2 - sysDate1);
+    }
+}
