62bf9ca16f1ad03a4144b665e2784363ff4d6c8e
Solomon Li <solomon-li@outlook.com>
2022-07-13 10:41:27 +0800

    L326_Power_of_Three

---
+++ src/L326_Power_of_Three.java
@@ -0,0 +1,34 @@
+import struct.ListNode;
+
+public class L326_Power_of_Three {
+
+    public boolean isPowerOfThree(int n) {
+        if(n <=0) {
+            return false;
+        }
+
+        if(n == 1) {
+            return true;
+        }
+
+        if(n%3 != 0) {
+            return false;
+        }
+
+        return isPowerOfThree(n/3);
+    }
+
+    public static void main(String[] args) {
+
+        L326_Power_of_Three s = new L326_Power_of_Three();
+
+        long sysDate1 = System.currentTimeMillis();
+
+        boolean x = s.isPowerOfThree(-3);
+        System.out.println(x);
+
+        long sysDate2 = System.currentTimeMillis();
+        System.out.println("\ntime ");
+        System.out.print(sysDate2 - sysDate1);
+    }
+}
