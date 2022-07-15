481c0cc062bdb20fd8b6e08bd50d69654f4e51e4
Solomon Li <solomon-li@outlook.com>
2022-07-15 17:28:52 +0800

    292. Nim Game

---
+++ src/L292_Nim_Game.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class L292_Nim_Game {
+
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+
+    public static void main(String[] args) {
+
+        L292_Nim_Game s = new L292_Nim_Game();
+        long sysDate1 = System.currentTimeMillis();
+
+        boolean res = s.canWinNim(6);
+        System.out.println(res);
+
+        long sysDate2 = System.currentTimeMillis();
+        System.out.println("\ntime ");
+        System.out.print(sysDate2 - sysDate1);
+    }
+}
