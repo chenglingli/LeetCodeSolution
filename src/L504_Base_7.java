21314fabef855875f3f3e1f96b70f802721bdae8
Solomon Li <solomon-li@outlook.com>
2022-09-20 10:19:36 +0800

    504. Base 7

---
+++ src/L504_Base_7.java
@@ -0,0 +1,42 @@
+import com.sun.tools.corba.se.idl.IncludeGen;
+
+import java.util.Arrays;
+
+public class L504_Base_7 {
+
+    public String convertToBase7(int num) {
+
+        if (num ==0) {
+            return "0";
+        }
+
+        StringBuilder res = new StringBuilder();
+
+        String negative = num < 0 ? "-":"";
+        num = Math.abs(num);
+        while (num > 0) {
+            int cur = num % 7;
+            num = num/7;
+            res.append(cur);
+        }
+
+        res.append(negative);
+
+        return res.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+
+        L504_Base_7 s = new L504_Base_7();
+        long sysDate1 = System.currentTimeMillis();
+        // int input = 101;
+        int input = -7;
+
+        String res = s.convertToBase7(input);
+        System.out.println(res);
+
+        long sysDate2 = System.currentTimeMillis();
+        System.out.println("\ntime ");
+        System.out.print(sysDate2 - sysDate1);
+    }
+}
