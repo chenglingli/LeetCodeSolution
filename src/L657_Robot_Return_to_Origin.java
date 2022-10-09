b8cd5cd181b21fd9d65d028adf26799ad9694dfc
Solomon Li <solomon-li@outlook.com>
2022-10-09 10:21:05 +0800

    657. Robot Return to Origin

---
+++ src/L657_Robot_Return_to_Origin.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class L657_Robot_Return_to_Origin {
+
+    public boolean judgeCircle(String moves) {
+        int h = 0;
+        int w = 0;
+        for (int i = 0 ; i < moves.length(); i++) {
+            if (moves.charAt(i) == 'U') {
+                h++;
+            }
+            if (moves.charAt(i) == 'D') {
+                h--;
+            }
+            if (moves.charAt(i) == 'R') {
+                w++;
+            }
+            if (moves.charAt(i) == 'L') {
+                w--;
+            }
+        }
+
+        return h ==0 && w ==0;
+    }
+
+    public static void main(String[] args) {
+
+        L657_Robot_Return_to_Origin s = new L657_Robot_Return_to_Origin();
+        long sysDate1 = System.currentTimeMillis();
+
+        String moves = "UU";
+        boolean res = s.judgeCircle(moves);
+        System.out.println(res);
+
+        long sysDate2 = System.currentTimeMillis();
+        System.out.println("\ntime ");
+        System.out.print(sysDate2 - sysDate1);
+    }
+}
