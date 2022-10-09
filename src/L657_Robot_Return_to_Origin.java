10fbcf93658f6e7f30ed344a6020ed0f01ff9dd7
Solomon Li <solomon-li@outlook.com>
2022-10-09 10:35:13 +0800

    refine

--- src/L657_Robot_Return_to_Origin.java
+++ src/L657_Robot_Return_to_Origin.java
@@ -7,18 +7,31 @@ public class L657_Robot_Return_to_Origin {
         int h = 0;
         int w = 0;
         for (int i = 0 ; i < moves.length(); i++) {
-            if (moves.charAt(i) == 'U') {
-                h++;
+
+//            if (moves.charAt(i) == 'U') {
+//                h++;
+//            }
+//            if (moves.charAt(i) == 'D') {
+//                h--;
+//            }
+//            if (moves.charAt(i) == 'R') {
+//                w++;
+//            }
+//            if (moves.charAt(i) == 'L') {
+//                w--;
+//            }
+
+            switch (moves.charAt(i)) {
+                case 'D':
+                    h--;break;
+                case 'U':
+                    h++;break;
+                case 'R':
+                    w--;break;
+                case 'L':
+                    w++;break;
             }
-            if (moves.charAt(i) == 'D') {
-                h--;
-            }
-            if (moves.charAt(i) == 'R') {
-                w++;
-            }
-            if (moves.charAt(i) == 'L') {
-                w--;
-            }
+
         }
 
         return h ==0 && w ==0;
@@ -29,7 +42,7 @@ public class L657_Robot_Return_to_Origin {
         L657_Robot_Return_to_Origin s = new L657_Robot_Return_to_Origin();
         long sysDate1 = System.currentTimeMillis();
 
-        String moves = "UU";
+        String moves = "UDLR";
         boolean res = s.judgeCircle(moves);
         System.out.println(res);
 
