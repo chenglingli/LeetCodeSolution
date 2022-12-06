724062ae9a4eb6752ef4544f187cff8e323ca4bc
Solomon Li <solomon-li@outlook.com>
2022-12-06 12:02:44 +0800

    L1281_Subtract_the_Product_and_Sum

---
+++ src/L1281_Subtract_the_Product_and_Sum.java
@@ -0,0 +1,34 @@
+public class L1281_Subtract_the_Product_and_Sum {
+
+    public int subtractProductAndSum(int n) {
+
+        if (n <= 9) {
+            return 0;
+        }
+
+        int copy = n;
+        int sum = 0;
+        int product = 1;
+        while (copy > 0) {
+            int temp = copy%10;
+            sum += temp;
+            product *= temp;
+            copy /= 10;
+        }
+        return product - sum;
+    }
+
+    public static void main(String[] args) {
+
+        L1281_Subtract_the_Product_and_Sum s = new L1281_Subtract_the_Product_and_Sum();
+        long sysDate1 = System.currentTimeMillis();
+
+        int input = 114;
+        int res = s.subtractProductAndSum(input);
+        System.out.println(res);
+
+        long sysDate2 = System.currentTimeMillis();
+        System.out.println("\ntime ");
+        System.out.print(sysDate2 - sysDate1);
+    }
+}
