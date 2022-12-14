1abbd5e1976d7c40d2d6a2b75f4cc00b00f365fb
Solomon Li <solomon-li@outlook.com>
2022-12-14 10:08:18 +0800

    L1304_Find_N_Unique_Integers_Sum_up_to_Zero

---
+++ src/L1304_Find_N_Unique_Integers_Sum_up_to_Zero.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class L1304_Find_N_Unique_Integers_Sum_up_to_Zero {
+
+    public int[] sumZero(int n) {
+        int x = n/2;
+        int [] res = new int[n];
+        int count = 0;
+        for (int i = x*(-1) ; i <= x ; i++) {
+            if (i == 0) {
+                continue;
+            }
+            res[count++] = i;
+        }
+
+        if (n %2 != 0) {
+            res[n-1] = 0;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        L1304_Find_N_Unique_Integers_Sum_up_to_Zero s = new L1304_Find_N_Unique_Integers_Sum_up_to_Zero();
+        long sysDate1 = System.currentTimeMillis();
+
+        int[] res = s.sumZero(5);
+        System.out.println(Arrays.toString(res));
+
+        long sysDate2 = System.currentTimeMillis();
+        System.out.println("\ntime ");
+        System.out.print(sysDate2 - sysDate1);
+    }
+}
