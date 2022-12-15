602d1f45ed91d6f211b4dc810f7ee4e129e01cbc
Solomon Li <solomon@Fogey.local>
2022-12-15 11:47:32 +0800

    1207. Unique Number of Occurrences

---
+++ src/L1207_Unique_Number_of_Occurrences.java
@@ -0,0 +1,45 @@
+public class L1207_Unique_Number_of_Occurrences {
+
+    /*
+
+    1 <= arr.length <= 1000
+    -1000 <= arr[i] <= 1000
+
+     */
+    public boolean uniqueOccurrences(int[] arr) {
+
+        int[] count = new int[2002];
+        int[] map = new int[1001];
+
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i] + 1000]++;
+        }
+
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] != 0) {
+                if (map[count[i]] == 1) {
+                    return false;
+                } else {
+                    map[count[i]] = 1;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        L1207_Unique_Number_of_Occurrences s = new L1207_Unique_Number_of_Occurrences();
+        long sysDate1 = System.currentTimeMillis();
+
+//        int[] arr = {-3, 0, 1, -3, 1, 1, 1, -3, 10, 0};
+        int[] arr = {1, 2};
+        boolean res = s.uniqueOccurrences(arr);
+        System.out.println(res);
+
+        long sysDate2 = System.currentTimeMillis();
+        System.out.println("\ntime ");
+        System.out.print(sysDate2 - sysDate1);
+    }
+}
