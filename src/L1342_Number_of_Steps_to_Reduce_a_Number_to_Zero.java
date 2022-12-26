1eafe4a606f3d84fc00e687ae135391549f6cd2c
Solomon Li <solomon-li@outlook.com>
2022-12-26 10:18:23 +0800

    L1342_Number_of_Steps_to_Reduce_a_Number_to_Zero

---
+++ src/L1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.java
@@ -0,0 +1,32 @@
+public class L1342_Number_of_Steps_to_Reduce_a_Number_to_Zero {
+
+    public int numberOfSteps(int num) {
+        int cur = num;
+        int count = 0;
+        while (cur != 0) {
+            if (cur %2 == 0) {
+                cur /= 2;
+            }
+            else {
+                cur --;
+            }
+            count++;
+        }
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+
+        L1342_Number_of_Steps_to_Reduce_a_Number_to_Zero s = new L1342_Number_of_Steps_to_Reduce_a_Number_to_Zero();
+        long sysDate1 = System.currentTimeMillis();
+
+        int num = 100000;
+        int res = s.numberOfSteps(num);
+        System.out.println(res);
+
+        long sysDate2 = System.currentTimeMillis();
+        System.out.println("\ntime ");
+        System.out.print(sysDate2 - sysDate1);
+    }
+}
