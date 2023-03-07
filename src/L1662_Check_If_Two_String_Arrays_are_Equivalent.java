ab7354067385136f0498ffc6840644294c22f673
Solomon Li <solomon-li@outlook.com>
2023-03-07 09:56:50 +0800

    1662. Check If Two String Arrays are Equivalent

---
+++ src/L1662_Check_If_Two_String_Arrays_are_Equivalent.java
@@ -0,0 +1,36 @@
+public class L1662_Check_If_Two_String_Arrays_are_Equivalent {
+
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder sb1 = new StringBuilder();
+        int i  = 0;
+        while ( i < word1.length) {
+            sb1.append(word1[i]);
+            i++;
+        }
+
+        StringBuilder sb2 = new StringBuilder();
+        i  = 0;
+        while ( i < word2.length) {
+            sb2.append(word2[i]);
+            i++;
+        }
+
+        return sb1.toString().equals(sb2.toString());
+    }
+
+    public static void main(String[] args) {
+
+        L1662_Check_If_Two_String_Arrays_are_Equivalent s = new L1662_Check_If_Two_String_Arrays_are_Equivalent();
+        long sysDate1 = System.currentTimeMillis();
+
+        String[] word1 = {"abc", "d", "defg"};
+        String[] word2 = {"abcddefg"};
+
+        boolean res = s.arrayStringsAreEqual(word1, word2);
+        System.out.println(res);
+
+        long sysDate2 = System.currentTimeMillis();
+        System.out.println("\ntime ");
+        System.out.print(sysDate2 - sysDate1);
+    }
+}
