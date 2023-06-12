539f6c32fb57dfb7025aaa428e20d678c06eaa19
Solomon Li <solomon-li@outlook.com>
2023-06-12 10:09:04 +0800

    1822. Sign of the Product of an Array

---
+++ src/L1822_Sign_of_the_Product_of_an_Array.java
@@ -0,0 +1,34 @@
+public class L1822_Sign_of_the_Product_of_an_Array {
+
+    public int arraySign(int[] nums) {
+
+        int count = 0;
+        for (int x : nums) {
+            if (x == 0) {
+                return 0;
+            }
+            if (x < 0) {
+                count++;
+            }
+        }
+
+        if (count % 2 == 0) {
+            return 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        L1822_Sign_of_the_Product_of_an_Array s = new L1822_Sign_of_the_Product_of_an_Array();
+        long sysDate1 = System.currentTimeMillis();
+
+        int[] nums = {-1, -2, -3, -4, 3, 2, 1};
+        int res = s.arraySign(nums);
+        System.out.println(res);
+
+        long sysDate2 = System.currentTimeMillis();
+        System.out.println("\ntime ");
+        System.out.print(sysDate2 - sysDate1);
+    }
+}
