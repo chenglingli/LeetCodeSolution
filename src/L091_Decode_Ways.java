be5e50866646329a90c704d43a6b875283c16f9f
Solomon Li <solomon-li@outlook.com>
2023-11-28 21:44:22 +0800

    refine comment

--- src/L091_Decode_Ways.java
+++ src/L091_Decode_Ways.java
@@ -5,11 +5,19 @@ public class L091_Decode_Ways {
      * 'B' -> "2"
      * ...
      * 'Z' -> "26"
-     * <p>
+     *
      * 解法
-     * 其中 count[i] 表示s中前i个字符组成的子串的解码方法的个数，长度比输入数组长多多1，并将 count[0] 初始化为1
-     * 只要 前一位 不为0，count[i]=count[i-1]
-     * 只要 前两位 在[10,26]之间，count[i]+=count[i-2]
+     * count[i] 表示s中前i个字符组成的子串的解码方法的个数，
+     *
+     * 初始化：
+     * 长度比输入数组长多多1，并将 count[0] 初始化为1
+     *
+     * 递归方程：
+     * 前一位 为'0'， count[i] = 0
+     * 前一位 不为'0'，count[i] = count[i-1]
+     * 前两位 在[10,26]之间，count[i] += count[i-2]
+     *
+     * 1234
      */
 
     public int numDecodings(String s) {
@@ -18,6 +26,7 @@ public class L091_Decode_Ways {
 
         count[0] = 1;
         for (int i = 1; i <= s.length(); i++) {
+
             if (s.charAt(i - 1) == '0') {
                 count[i] = 0;
             } else {
