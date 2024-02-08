6b14b81a6a00b19ac6cb65c537eb879093fb1f1a
Solomon Li <solomon-li@outlook.com>
2024-02-08 21:49:23 +0800

    313. Super Ugly Number

--- src/L264_Ugly_Number_II.java
+++ src/L264_Ugly_Number_II.java
@@ -6,19 +6,18 @@ public class L264_Ugly_Number_II {
         if (n == 1)
             return 1; // base case
 
-        int t2 = 0, t3 = 0, t5 = 0; //pointers for 2, 3, 5
-
-        int[] dp = new int[n];
-        dp[0] = 1;
+        int [] index = new int[6];
+        int[] ugly = new int[n];
+        ugly[0] = 1;
 
         for (int i = 1; i < n; i++) {
-            dp[i] = Math.min(dp[t2] * 2, Math.min(dp[t3] * 3, dp[t5] * 5));
-            if (dp[i] == dp[t2] * 2) t2++;
-            if (dp[i] == dp[t3] * 3) t3++;
-            if (dp[i] == dp[t5] * 5) t5++;
+            ugly[i] = Math.min(ugly[index[2]] * 2, Math.min(ugly[index[3]] * 3, ugly[index[5]] * 5));
+            if (ugly[i] == ugly[index[2]] * 2) index[2]++;
+            if (ugly[i] == ugly[index[3]] * 3) index[3]++;
+            if (ugly[i] == ugly[index[5]] * 5) index[5]++;
         }
 
-        return dp[n - 1];
+        return ugly[n - 1];
     }
 
     public static void main(String[] args) {
@@ -26,7 +25,7 @@ public class L264_Ugly_Number_II {
         L264_Ugly_Number_II s = new L264_Ugly_Number_II();
         long sysDate1 = System.currentTimeMillis();
 
-        int res = s.nthUglyNumber(496);
+        int res = s.nthUglyNumber(10);
         System.out.println(res);
 
         long sysDate2 = System.currentTimeMillis();
