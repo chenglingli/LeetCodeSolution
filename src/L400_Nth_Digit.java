d1e783e2e497a0baa07b028aa2f4ae9fdf60f67a
Solomon Li <solomon-li@outlook.com>
2024-04-03 12:36:25 +0800

    findNthDigit2

--- src/L400_Nth_Digit.java
+++ src/L400_Nth_Digit.java
@@ -62,6 +62,25 @@ public class L400_Nth_Digit {
         return des % 10;
     }
 
+
+    public int findNthDigit2(int n) {
+
+        int len = 1;
+        long count = 9;
+        int start = 1;
+
+        while (n > len * count) {
+            n -= len * count;
+            len += 1;
+            count *= 10;
+            start *= 10;
+        }
+
+        start += (n - 1) / len;
+        String s = Integer.toString(start);
+        return s.charAt((n - 1) % len) - '0';
+    }
+
     public static void main(String[] args) {
 
         L400_Nth_Digit s = new L400_Nth_Digit();
