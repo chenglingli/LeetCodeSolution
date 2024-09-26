189fc24a54df622b8a493434616780cf5313f29c
Solomon Li <solomon-li@outlook.com>
2024-09-26 11:52:40 +0800

    183. Customers Who Never Order

---
+++ src/L183_Customers_Who_Never_Order.sql
@@ -0,0 +1,73 @@
+Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID and name of a customer.
+
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+customerId is a foreign key (reference columns) of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+
+
+Write a solution to find all customers who never order anything.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output:
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+
+-- 联表查询（更快一些）
+select A.name as Customers
+from Customers as A
+left join Orders as B
+on A.id = B.customerId
+where B.customerId is null
+
+
+-- 也可以嵌套查询
+Select name as Customers from customers
+where id not in
+(select distinct customerId from orders)
