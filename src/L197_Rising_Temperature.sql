05d9cb7daf7227ebdecabde3e41647cca89aeabf
Solomon Li <solomon-li@outlook.com>
2024-10-10 14:54:40 +0800

    197. Rising Temperature

---
+++ src/L197_Rising_Temperature.sql
@@ -0,0 +1,73 @@
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+There are no different rows with the same recordDate.
+This table contains information about the temperature on a certain day.
+
+
+Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output:
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation:
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+
+-- 1581 ms
+select id from Weather
+where temperature >
+(select temperature from Weather as w2 where w2.recordDate = date_sub(Weather.recordDate, interval 1 day));
+
+
+-- 353ms
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2 ON DATE(w1.recordDate) = DATE(w2.recordDate) + INTERVAL 1 DAY
+WHERE w1.temperature > w2.temperature;
+
+
+-- 482ms
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+    ON DATE(w1.recordDate) = DATE(w2.recordDate) + INTERVAL 1 DAY
+    and w1.temperature > w2.temperature;
+
+
+-- 464ms
+SELECT today.id
+FROM Weather yesterday
+CROSS JOIN Weather today
+WHERE DATEDIFF(today.recordDate, yesterday.recordDate) = 1
+    AND today.temperature > yesterday.temperature;
