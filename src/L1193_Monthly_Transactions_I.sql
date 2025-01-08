55c8355bac635142f41e62a85f869f0c7c642ed1
Solomon Li <solomon-li@outlook.com>
2025-01-08 10:47:41 +0800

    1193. Monthly Transactions I

---
+++ src/L1193_Monthly_Transactions_I.sql
@@ -0,0 +1,84 @@
+Table: Transactions
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| country       | varchar |
+| state         | enum    |
+| amount        | int     |
+| trans_date    | date    |
++---------------+---------+
+id is the primary key of this table.
+The table has information about incoming transactions.
+The state column is an enum of type ["approved", "declined"].
+
+
+Write an SQL query to find for each month and country, the number of transactions and their total amount,
+the number of approved transactions and their total amount.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Transactions table:
++------+---------+----------+--------+------------+
+| id   | country | state    | amount | trans_date |
++------+---------+----------+--------+------------+
+| 121  | US      | approved | 1000   | 2018-12-18 |
+| 122  | US      | declined | 2000   | 2018-12-19 |
+| 123  | US      | approved | 2000   | 2019-01-01 |
+| 124  | DE      | approved | 2000   | 2019-01-07 |
++------+---------+----------+--------+------------+
+Output:
++----------+---------+-------------+----------------+--------------------+-----------------------+
+| month    | country | trans_count | approved_count | trans_total_amount | approved_total_amount |
++----------+---------+-------------+----------------+--------------------+-----------------------+
+| 2018-12  | US      | 2           | 1              | 3000               | 1000                  |
+| 2019-01  | US      | 1           | 1              | 2000               | 2000                  |
+| 2019-01  | DE      | 1           | 1              | 2000               | 2000                  |
++----------+---------+-------------+----------------+--------------------+-----------------------+
+
+select
+    DATE_FORMAT(trans_date, '%Y-%m') as month,
+    country,
+    count(id) as trans_count,
+    sum(case when state = 'approved' then 1 else 0 end) as approved_count,
+    sum(amount) as trans_total_amount,
+    sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+from
+    Transactions
+group by
+    month,country
+
+
+
+select
+    substring(trans_date,1,7) month, country,
+    count(*) as trans_count,
+    sum(if(state = 'approved',1,0)) approved_count,
+    sum(amount) trans_total_amount,
+    sum(if(state = 'approved',amount,0)) approved_total_amount
+from
+    transactions
+GROUP BY
+    month, country;
+
+
+SELECT
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    country,
+    COUNT(*) AS trans_count,
+    COUNT(IF(state = 'approved', 1, NULL)) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
+FROM
+    Transactions
+GROUP BY
+    month,
+    country
