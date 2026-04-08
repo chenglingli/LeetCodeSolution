9183fd3e70945707bca602dde19a0d30cee77597
Solomon Li <solomon-li@outlook.com>
2026-04-08 11:41:00 +0800

    3884. First Matching Character From Both Ends

---
+++ src/L3884_First_Matching_Character_From_Both_Ends.java
@@ -0,0 +1,79 @@
+public class L3884_First_Matching_Character_From_Both_Ends {
+
+    /*
+    You are given a string s of length n consisting of lowercase English letters.
+
+    Return the smallest index i such that s[i] == s[n - i - 1].
+
+    If no such index exists, return -1.
+
+
+
+    Example 1:
+
+    Input: s = "abcacbd"
+
+    Output: 1
+
+    Explanation:
+
+    At index i = 1, s[1] and s[5] are both 'b'.
+
+    No smaller index satisfies the condition, so the answer is 1.
+
+    Example 2:
+
+    Input: s = "abc"
+
+    Output: 1
+
+    Explanation:
+
+    At index i = 1, the two compared positions coincide, so both characters are 'b'.
+
+    No smaller index satisfies the condition, so the answer is 1.
+
+    Example 3:
+
+    Input: s = "abcdab"
+
+    Output: -1
+
+    Explanation:
+
+    For every index i, the characters at positions i and n - i - 1 are different.
+
+    Therefore, no valid index exists, so the answer is -1.
+
+
+
+    Constraints:
+
+    1 <= n == s.length <= 100
+    s consists of lowercase English letters.
+
+     */
+    public int firstMatchingIndex(String s) {
+        int len = s.length();
+        for (int i = 0; i < len / 2 + 1; i++) {
+            if (s.charAt(i) == s.charAt(len - i - 1)) return i;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        L3884_First_Matching_Character_From_Both_Ends s = new L3884_First_Matching_Character_From_Both_Ends();
+        long sysDate1 = System.currentTimeMillis();
+
+        String ss = "abc";
+
+        int res = s.firstMatchingIndex(ss);
+        System.out.println(res);
+
+        long sysDate2 = System.currentTimeMillis();
+        System.out.println("\ntime ");
+        System.out.print(sysDate2 - sysDate1);
+    }
+}
